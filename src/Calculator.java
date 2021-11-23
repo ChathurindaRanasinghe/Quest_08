e6470aaa3769d081271bf795ba6fc377e7b9eb85
Chathurinda Ranasinghe <chathurindaranasinghe@gmail.com>
2021-11-23 20:12:15 +0530

    updated

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,16 +8,20 @@ import java.util.Stack;
 
 public class Calculator implements ActionListener {
 
+    /** Constants required for GUI*/
     final int fontSize = 20;
     final Font font = new Font("SFUIDisplay-Regular", Font.PLAIN, fontSize);
     final Font font2 = new Font("SFUIDisplay-Regular", Font.PLAIN, 20);
     final Color color = new Color(191, 245, 239);
     final Color color2 = new Color(142, 236, 216);
 
-    JFrame frame;//Creating object of JFrame class
+    /** Creating the main window for the calculator */
+    JFrame frame;
 
+    /** Display of the calculator */
     JTextField textField = new JTextField();
 
+    /** Buttons needed for the calculator */
     JButton buttonZero = new JButton("0");
     JButton buttonOne = new JButton("1");
     JButton buttonTwo = new JButton("2");
@@ -39,7 +43,6 @@ public class Calculator implements ActionListener {
     JButton buttonMod = new JButton("%");
     JButton buttonOpenParenthesis = new JButton("(");
     JButton buttonClosedParenthesis = new JButton(")");
-
     JButton buttonSin = new JButton("sin");
     JButton buttonCos = new JButton("cos");
     JButton buttonTan = new JButton("tan");
@@ -55,8 +58,15 @@ public class Calculator implements ActionListener {
         addActionEvents();
     }
 
+
+    /**
+     * Builds the main frame for the calculator
+     */
     public void prepareGUI() {
         frame = new JFrame();
+
+        Image icon = Toolkit.getDefaultToolkit().getImage("icon.png");
+        frame.setIconImage(icon);
 
         frame.setTitle("Calculator");                           //Setting title of the JFrame
         frame.setSize(380, 675);                    //Setting size
@@ -68,6 +78,10 @@ public class Calculator implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //Setting default close operation
     }
 
+
+    /**
+     * Builds the button and the display in the GUI.
+     */
     public void addComponents() {
 
         textField.setBounds(0, 0, 370, 160);
@@ -242,6 +256,9 @@ public class Calculator implements ActionListener {
 
     }
 
+    /**
+     * Registers a button press.
+     */
     public void addActionEvents() {
 
         buttonZero.addActionListener(this);
@@ -274,6 +291,9 @@ public class Calculator implements ActionListener {
 
     }
 
+    /**
+     * @param e Action activated by pressing the button.
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         Object source = e.getSource();
@@ -332,11 +352,12 @@ public class Calculator implements ActionListener {
             String str = textField.getText();
 
             try {
-                String ans = extractTrig(str);
+                String ans = evaluateTrigFunction(str);
                 textField.setText(ans);
 
             } catch (Exception exception) {
                 textField.setText("Error");
+                exception.printStackTrace();
             }
 
         } else if (source == buttonSin) {
@@ -355,7 +376,11 @@ public class Calculator implements ActionListener {
 
     }
 
-    public static double precedence(char op) {
+    /**
+     * @param op operator
+     * @return value according to the precedence (higher the precedence higher the value)
+     */
+    public static int precedence(char op) {
         if (op == '+' || op == '-')
             return 1;
         if (op == '*' || op == '/' || op == '%')
@@ -363,40 +388,53 @@ public class Calculator implements ActionListener {
         return 0;
     }
 
-    public static double applyOp(double a, double b, char op) {
+    /**
+     * @param a left value
+     * @param b right value
+     * @param op operator
+     * @return performs the given operation according to the operator
+     */
+    public static double applyOp(double a, double b, char op) throws Exception {
         return switch (op) {
             case '+' -> a + b;
             case '-' -> a - b;
             case '*' -> a * b;
             case '/' -> a / b;
             case '%' -> a % b;
-            default -> 0;
+            default -> throw new Exception("Error!");
         };
     }
 
-    public static char[] plusMinus(String tokens) {
-        tokens = tokens.replace("++", "+");
-        tokens = tokens.replace("+-", "-");
-        tokens = tokens.replace("-+", "-");
-        tokens = tokens.replace("--", "+");
+    /**
+     * Constructs the expression so that negative numbers and multiple operators can be handled.
+     * Example : -0.6 + 36 is not works in evaluation since '-' is treated as an operator. Thus the expression
+     * is converted to (0-0.6)+36.
+     * @param expression expression that need to be processed
+     * @return constructed expression.
+     */
+    public static char[] plusMinus(String expression) {
+        expression = expression.replace("++", "+");
+        expression = expression.replace("+-", "-");
+        expression = expression.replace("-+", "-");
+        expression = expression.replace("--", "+");
 
         int end = -1;
 
-        StringBuilder str = new StringBuilder(tokens);
-        if (tokens.charAt(0) == '+') {
+        StringBuilder str = new StringBuilder(expression);
+        if (expression.charAt(0) == '+') {
             str.deleteCharAt(0);
-        } else if (tokens.charAt(0) == '-') {
+        } else if (expression.charAt(0) == '-') {
 
-            for (int i = 1; i < tokens.length(); i++) {
-                if (!Character.isDigit(tokens.charAt(i)) && tokens.charAt(i) != 'c' && tokens.charAt(i) != 'o' && tokens.charAt(i) != 's' && tokens.charAt(i) != 'i' && tokens.charAt(i) != 'n' && tokens.charAt(i) != 'a' && tokens.charAt(i) != 't'
+            for (int i = 1; i < expression.length(); i++) {
+                if (!Character.isDigit(expression.charAt(i)) && expression.charAt(i) != 'c' && expression.charAt(i) != 'o' && expression.charAt(i) != 's' && expression.charAt(i) != 'i' && expression.charAt(i) != 'n' && expression.charAt(i) != 'a' && expression.charAt(i) != 't'
                 ) {
-                    if (tokens.charAt(i) != '.') {
+                    if (expression.charAt(i) != '.') {
                         end = i;
 
                         break;
                     }
                 }
-                if (i == tokens.length() - 1) end = tokens.length();
+                if (i == expression.length() - 1) end = expression.length();
 
             }
             str.insert(end, ')');
@@ -434,26 +472,31 @@ public class Calculator implements ActionListener {
         return str.toString().toCharArray();
     }
 
-    public static double evaluate(String tokens) {
+    /**
+     * Evaluates the expressions that does not contain any trigonometry functions.
+     * @param expression expression that needs to be evaluated.
+     * @return result of the expression after calculation.
+     */
+    public static double evaluate(String expression) throws Exception {
         int i;
 
-        char[] token = plusMinus(tokens);
+        char[] expressionArr = plusMinus(expression);
 
         Stack<Double> values = new Stack<>();
 
         Stack<Character> ops = new Stack<>();
         ops.push('(');
 
-        for (i = 0; i < token.length; i++) {
+        for (i = 0; i < expressionArr.length; i++) {
 
-            if (token[i] == '(') {
-                ops.push(token[i]);
-            } else if (Character.isDigit(token[i])) {
+            if (expressionArr[i] == '(') {
+                ops.push(expressionArr[i]);
+            } else if (Character.isDigit(expressionArr[i])) {
                 StringBuilder val = new StringBuilder();
 
-                while (i < token.length && (Character.isDigit(token[i]) || token[i] == '.')) {
-                    if (Character.isDigit(token[i]))
-                        val.append((token[i] - '0'));
+                while (i < expressionArr.length && (Character.isDigit(expressionArr[i]) || expressionArr[i] == '.')) {
+                    if (Character.isDigit(expressionArr[i]))
+                        val.append((expressionArr[i] - '0'));
                     else {
                         val.append(".");
                     }
@@ -462,7 +505,7 @@ public class Calculator implements ActionListener {
                 }
                 values.push(Double.parseDouble(val.toString()));
                 i--;
-            } else if (token[i] == ')') {
+            } else if (expressionArr[i] == ')') {
                 while (!ops.empty() && ops.peek() != '(') {
                     double val2 = values.peek();
                     values.pop();
@@ -479,7 +522,7 @@ public class Calculator implements ActionListener {
                 if (!ops.empty())
                     ops.pop();
             } else {
-                while (!ops.empty() && precedence(ops.peek()) >= precedence(token[i])) {
+                while (!ops.empty() && precedence(ops.peek()) >= precedence(expressionArr[i])) {
                     double val2 = values.peek();
                     values.pop();
 
@@ -492,11 +535,11 @@ public class Calculator implements ActionListener {
                     values.push(applyOp(val1, val2, op));
                 }
 
-                ops.push(token[i]);
+                ops.push(expressionArr[i]);
             }
         }
 
-        while (ops.size() > 1) {
+        while (ops.size()>0 && values.size()>1) {
             double val2 = values.peek();
             values.pop();
 
@@ -509,30 +552,47 @@ public class Calculator implements ActionListener {
             values.push(applyOp(val1, val2, op));
         }
 
+        if(values.size() != 1 && ops.size()!=1){
+            throw new Exception("Error!");
+        }
+
         return values.peek();
     }
 
-    public static String extractTrig(String s) {
-        char[] str = s.toCharArray();
+    /**
+     * @param expression expression needs to be processed
+     * @return  result of the expression
+     * @throws Exception If the expression syntax wrong
+     */
+    public static String evaluateTrigFunction(String expression) throws Exception {
+        char[] expressionArr = expression.toCharArray();
 
         Stack<Character> stack = new Stack<>();
 
-        int n = s.length();
-        boolean flag = true;
+        int n = expression.length();
+        boolean noTrigContains = true;
+
         String trigF = "";
-        String extracted = "";
+        String expressionInsideTrigFunction = "";
+
         int index = -1, start = -1;
         for (int i = 0; i < n; i++) {
 
-            if (str[i] == 'a' && str[i + 1] == 'n') {
+            if(i!=0 && Character.isAlphabetic(expressionArr[i])){
+                if(Character.isDigit(expressionArr[i-1])){
+                    throw new Exception("Error!");
+                }
+            }
+
+            if (expressionArr[i] == 'a' && expressionArr[i + 1] == 'n') {
                 trigF = "tan";
                 index = i + 2;
                 start = i + 2;
-                flag = false;
-            } else if (str[i] == 'a') {
-                if (str[i + 1] == 's') {
+                noTrigContains = false;
+            } else if (expressionArr[i] == 'a') {
+                if (expressionArr[i + 1] == 's') {
                     trigF = "asin";
-                } else if (str[i + 1] == 'c') {
+                } else if (expressionArr[i + 1] == 'c') {
                     trigF = "acos";
                 } else {
                     trigF = "atan";
@@ -540,38 +600,38 @@ public class Calculator implements ActionListener {
 
                 index = i + 4;
                 start = i + 4;
-                flag = false;
-            } else if (str[i] == 'i' && str[i + 1] == 'n') {
+                noTrigContains = false;
+            } else if (expressionArr[i] == 'i' && expressionArr[i + 1] == 'n') {
                 trigF = "sin";
                 index = i + 2;
                 start = i + 2;
-                flag = false;
-            } else if (str[i] == 'o' && str[i + 1] == 's') {
+                noTrigContains = false;
+            } else if (expressionArr[i] == 'o' && expressionArr[i + 1] == 's') {
                 trigF = "cos";
                 index = i + 2;
                 start = i + 2;
-                flag = false;
+                noTrigContains = false;
             }
 
-            if (!flag) {
-                stack.push(str[index]);
+            if (!noTrigContains) {
+                stack.push(expressionArr[index]);
                 while (!stack.empty()) {
                     index++;
-                    if (str[index] == ')') stack.pop();
-                    if (str[index] == '(') stack.push('(');
+                    if (expressionArr[index] == ')') stack.pop();
+                    if (expressionArr[index] == '(') stack.push('(');
 
                 }
 
-                extracted = s.substring(start + 1, index);
-                s = s.replace(trigF + "(" + extracted + ")", trigF + "()");
+                expressionInsideTrigFunction = expression.substring(start + 1, index);
+                expression = expression.replace(trigF + "(" + expressionInsideTrigFunction + ")", trigF + "()");
                 break;
             }
         }
 
-        if (flag) {
-            return Double.toString(evaluate(s));
+        if (noTrigContains) {
+            return Double.toString(evaluate(expression));
         } else {
-            double v = Double.parseDouble(extractTrig(extracted));
+            double v = Double.parseDouble(evaluateTrigFunction(expressionInsideTrigFunction));
             double value = 0;
             switch (trigF) {
                 case "sin" -> value = Math.sin(Math.toRadians(v));
@@ -583,11 +643,11 @@ public class Calculator implements ActionListener {
                 default -> System.out.println("invalid Trig Function");
             }
             String vstr = new BigDecimal(value).toPlainString();
-            s = s.replace(trigF + "()", vstr);
+            expression = expression.replace(trigF + "()", vstr);
         }
 
-        s = s.replace("+-", "-");
-        return extractTrig(s);
+        expression = expression.replace("+-", "-");
+        return evaluateTrigFunction(expression);
 
     }
 
